2facf9b221aec25da497206883f1d20825573351
Greg C <gregcourt10@gmail.com>
2023-01-26 15:22:21 +0000

    first commit

---
+++ src/main/java/Guest.java
@@ -0,0 +1,12 @@
+public class Guest {
+
+    // Properties
+    private String name;
+
+    // Class Constructor
+    public Guest(String name){
+        this.name = name;
+    }
+
+
+}
---
+++ src/main/java/Server.java
@@ -0,0 +1,12 @@
+public class Server {
+    // Properties
+
+    // Class Constructor
+    public Server() {
+
+    }
+    // Methods
+    public boolean canServeGuest(Guest guest){
+        return true;
+    }
+}
